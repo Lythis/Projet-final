1318e2d94c453b80a38ec0d6487b80f9340c0665
NicoYam <givsoul@gmail.com>
2020-03-16 13:49:17 +0100

    Initialisation du projet

---
+++ QuestionsReponses.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="CSS/style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Poser une question</title>
+	</head>
+	<body>
+
+		<div class="card responsive-bootstrap-card mx-auto">
+			<h5 class="card-header"><b>Lythis</b> a posé la question</h5>
+			<div class="card-body">
+				<h5 class="card-title">Anime</h5>
+				<p class="card-text">Pourquoi les waifus ne sont qu'en 2D?</p>
+				<a href="#" class="btn btn-primary">Afficher les réponses</a>
+			</div>
+		</div>
+
+	</body>
+	<script src="https://kit.fontawesome.com/6c2421ea48.js" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</html>
