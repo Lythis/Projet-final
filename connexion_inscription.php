09e0be2ae542e2a31440e1c8774896a42d795d2b
Kyllian <lj.kyllian@gmail.com>
2020-03-22 16:00:05 +0100

    ajout de la page inscription

---
+++ connexion_inscription.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style_haut.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>connextion</title>
+    </head>
+    <body class="inscription">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">connexion</h5>
+                        <form>
+                            <div class="form-group">
+                                <label for="InputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="entre ton email">
+                                <small id="emailHelp" class="form-text text-muted"></small>
+                            </div>
+                            <div class="form-group">
+                                <label for="InputPassword1">mot de passe</label>
+                                <input type="password" class="form-control" id="InputPassword1" placeholder="MDP">
+                            </div>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">pas de compte, alors inscris toi</h5>
+                        <p class="card-text">inscris toi afin de poser des questions</p>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#inscription">
+                            inscription
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="inscription" tabindex="-1" role="dialog" aria-labelledby="inscriptionLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="inscriptionLabel">Page inscription</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="InputPseudo">pseudo</label>
+                                                <input type="email" class="form-control" id="pseudo" aria-describedby="pseudoHelp" placeholder="ecrit ton pseudo.">
+                                                <small id="pseudoHelp" class="form-text text-muted"></small>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="InputEmail1">Email address</label>
+                                                <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="entre ton email">
+                                                <small id="emailHelp" class="form-text text-muted"></small>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="InputPassword1">mot de passe</label>
+                                                <input type="password" class="form-control" id="InputPassword1" placeholder="MDP">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="InputPassword1">confirme ton MDP</label>
+                                                <input type="password" class="form-control" id="InputPassword1" placeholder="confirme ton MDP">
+                                            </div>
+
+
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">fermer</button>
+                                        <button type="submit" class="btn btn-primary">inscription</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    </body>
+</html>
