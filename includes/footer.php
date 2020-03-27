e21be420c47d815a4fa548cb73222e5e46f01666
Kyllian <lj.kyllian@gmail.com>
2020-03-27 15:10:53 +0100

    modif

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<<<<<<< HEAD
+	<link rel="stylesheet" type="text/css" href="../css/index.css">
+=======
+<<<<<<< HEAD
+	<link rel="stylesheet" type="text/css" href="../css/index.css">
+=======
+	<link rel="stylesheet" type="text/css" href="./CSS/style.css">
+>>>>>>> ad1e811ed90a9660b3490381f901934a50a42883
+>>>>>>> 994aa15a2e57ca499383b13f09052097e4effcc0
+	 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
+    <script src="https://kit.fontawesome.com/6c2421ea48.js" crossorigin="anonymous"></script>
+</head>
 <footer>
 	<div class="creux"></div>
 	<div class="footer">
@@ -14,8 +32,7 @@
 				<i class="fab fa-google"></i>
 			</div>
 		</div>
-	</div>	
+		</div>	
 	</div>	
 </footer>
-
-
+</html>
--- profile.php
+++ profile.php
@@ -8,34 +8,27 @@
 		<title>Profil de Lythis</title>
 	</head>
 	<body>
-		<?php
-			require_once('includes/nav-bar.php'); 
-		?>
-		<div class="card responsive-bootstrap-card mx-5">
-			<div class="row_ligne card-header">
-				<div class="container_profil">
+		<div class="card responsive-bootstrap-card mx-auto">
+			<div class="container">
+				<div class="row card-header">
 			    	<div class="col-3">
-			      			<img class="rounded float-left img-fluid mr-4" src="https://i.pinimg.com/236x/1b/50/3b/1b503bafe77169ec44e0080964bb6e51.jpg" alt="">
-			    	</div>	    
-			    	<div class="col ">
-			      		<h3 class="card-title ml-2">Lythis</h3>
+			      		<img class="rounded float-left img-fluid" src="https://i.pinimg.com/236x/1b/50/3b/1b503bafe77169ec44e0080964bb6e51.jpg" alt="">
+			    	</div>
+			    	<div class="col">
+			      		<h3 class="card-title">Lythis</h3>
 			  			<h5>Genre : Homme</h5>
 			  			<p>Description personnelle : Je suis un dévelopeur full-stack.</p>
 			    	</div>
-
 				</div>
-				<div class="row_ligne card-body">
+				<div class="row card-body">
 			  		<div class="col">
-			    		<h5 class="card-title ">Questions posées par Lythis :</h5>
+			    		<h5 class="card-title">Questions posées par Lythis :</h5>
 			    		<p class="card-text">Wow, such empty.</p>
 			  		</div>
 			  	</div>
 			</div>
 		</div>
-	<?php 
-	require_once('./includes/footer.php');
 
-	?>
 	</body>
 	<script src="https://kit.fontawesome.com/6c2421ea48.js" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
