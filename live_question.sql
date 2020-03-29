84162fc68eee0455d51c86e49fcfc0469ac129ab
NicoYam <givsoul@gmail.com>
2020-03-29 10:30:11 +0200

    Ajout de la base de données avec root

--- live_question.sql
+++ live_question.sql
@@ -1,26 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Ven 14 Février 2020 à 17:02
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 29 mars 2020 à 08:29
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `live_question`
 --
-CREATE DATABASE IF NOT EXISTS `live_question` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `live_question`;
 
 -- --------------------------------------------------------
 
@@ -28,11 +28,12 @@ USE `live_question`;
 -- Structure de la table `categorie`
 --
 
+DROP TABLE IF EXISTS `categorie`;
 CREATE TABLE IF NOT EXISTS `categorie` (
-    `Id_categorie` int(11) NOT NULL AUTO_INCREMENT,
-    `Libelle_categorie` varchar(255) CHARACTER SET latin1 NOT NULL,
-    PRIMARY KEY (`Id_categorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `Id_categorie` int(11) NOT NULL AUTO_INCREMENT,
+  `Libelle_categorie` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`Id_categorie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -40,16 +41,24 @@ CREATE TABLE IF NOT EXISTS `categorie` (
 -- Structure de la table `profil`
 --
 
+DROP TABLE IF EXISTS `profil`;
 CREATE TABLE IF NOT EXISTS `profil` (
-    `Id_profil` int(11) NOT NULL AUTO_INCREMENT,
-    `Pseudo_profil` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `Mail_profil` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `MotDePasse_profil` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `Genre_profil` varchar(50) CHARACTER SET latin1 NOT NULL,
-    `#Id_role` int(11) NOT NULL,
-    PRIMARY KEY (`Id_profil`),
-    KEY `#Id_role` (`#Id_role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `Id_profil` int(11) NOT NULL AUTO_INCREMENT,
+  `Pseudo_profil` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `Mail_profil` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `MotDePasse_profil` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `Genre_profil` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `#Id_role` int(11) NOT NULL,
+  PRIMARY KEY (`Id_profil`),
+  KEY `#Id_role` (`#Id_role`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `profil`
+--
+
+INSERT INTO `profil` (`Id_profil`, `Pseudo_profil`, `Mail_profil`, `MotDePasse_profil`, `Genre_profil`, `#Id_role`) VALUES
+(1, 'root', 'root@livequestion.com', '12345', 'Non binaire', 1);
 
 -- --------------------------------------------------------
 
@@ -57,16 +66,17 @@ CREATE TABLE IF NOT EXISTS `profil` (
 -- Structure de la table `question`
 --
 
+DROP TABLE IF EXISTS `question`;
 CREATE TABLE IF NOT EXISTS `question` (
-    `Id_question` int(11) NOT NULL AUTO_INCREMENT,
-    `Titre_question` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `Date_creation_question` date NOT NULL,
-    `#Id_profil` int(11) NOT NULL,
-    `#Id_categorie` int(11) NOT NULL,
-    PRIMARY KEY (`Id_question`),
-    KEY `#Id_profil` (`#Id_profil`),
-    KEY `#Id_categorie` (`#Id_categorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `Id_question` int(11) NOT NULL AUTO_INCREMENT,
+  `Titre_question` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `Date_creation_question` date NOT NULL,
+  `#Id_profil` int(11) NOT NULL,
+  `#Id_categorie` int(11) NOT NULL,
+  PRIMARY KEY (`Id_question`),
+  KEY `#Id_profil` (`#Id_profil`),
+  KEY `#Id_categorie` (`#Id_categorie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -74,16 +84,17 @@ CREATE TABLE IF NOT EXISTS `question` (
 -- Structure de la table `reponse`
 --
 
+DROP TABLE IF EXISTS `reponse`;
 CREATE TABLE IF NOT EXISTS `reponse` (
-    `Id_reponse` int(11) NOT NULL AUTO_INCREMENT,
-    `Contenu_reponse` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `Date_reponse` date NOT NULL,
-    `#Id_profil` int(11) NOT NULL,
-    `#Id_question` int(11) NOT NULL,
-    PRIMARY KEY (`Id_reponse`),
-    KEY `#Id_profil` (`#Id_profil`),
-    KEY `#Id_question` (`#Id_question`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `Id_reponse` int(11) NOT NULL AUTO_INCREMENT,
+  `Contenu_reponse` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `Date_reponse` date NOT NULL,
+  `#Id_profil` int(11) NOT NULL,
+  `#Id_question` int(11) NOT NULL,
+  PRIMARY KEY (`Id_reponse`),
+  KEY `#Id_profil` (`#Id_profil`),
+  KEY `#Id_question` (`#Id_question`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -91,14 +102,23 @@ CREATE TABLE IF NOT EXISTS `reponse` (
 -- Structure de la table `role`
 --
 
+DROP TABLE IF EXISTS `role`;
 CREATE TABLE IF NOT EXISTS `role` (
-    `Id_role` int(11) NOT NULL AUTO_INCREMENT,
-    `Libelle_role` varchar(50) CHARACTER SET latin1 NOT NULL,
-    PRIMARY KEY (`Id_role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `Id_role` int(11) NOT NULL AUTO_INCREMENT,
+  `Libelle_role` varchar(50) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`Id_role`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
--- Contraintes pour les tables exportées
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`Id_role`, `Libelle_role`) VALUES
+(1, 'Admin'),
+(2, 'Utilisateur');
+
+--
+-- Contraintes pour les tables déchargées
 --
 
 --
@@ -120,6 +140,7 @@ ALTER TABLE `question`
 ALTER TABLE `reponse`
   ADD CONSTRAINT `FK_#Id_profil2` FOREIGN KEY (`#Id_profil`) REFERENCES `profil` (`Id_profil`),
   ADD CONSTRAINT `FK_#Id_question` FOREIGN KEY (`#Id_question`) REFERENCES `question` (`Id_question`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
