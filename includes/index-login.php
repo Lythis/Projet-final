fe5a24bbc0b815ecd43670948ecd3623c312b290
NicoYam <givsoul@gmail.com>
2020-04-06 17:07:09 +0200

    Modification du PHP et utilisation de requêtes imbriquées

--- includes/index-login.php
+++ includes/index-login.php
@@ -5,35 +5,34 @@
         $query->execute();
         $questions = $query->fetchAll();
 
-        $query = $con->prepare("SELECT * FROM profil");
-        $query->execute();
-        $users = $query->fetchAll();
-
-        $query = $con->prepare("SELECT * FROM categorie");
-        $query->execute();
-        $categorie = $query->fetchAll();
-
         foreach ($questions as $question) {
 
-            $reponsequestion = $question['Id_question'];
+            $idquestion = $question["Id_question"];
+            $idprofil = $question["#Id_profil"];
+            $idcategorie = $question["#Id_categorie"];
 
-            $query = $con->prepare("SELECT * FROM `reponse` WHERE `#Id_question` = $reponsequestion");
+            $query = $con->prepare("SELECT * FROM `reponse` WHERE `#Id_question` = ( SELECT `Id_question` FROM `question` WHERE `Id_question` = $idquestion ORDER BY `Date_reponse` )");
             $query->execute();
             $reponses = $query->fetchAll();
 
-            $categoriequestion = $question["#Id_categorie"] - 1;
-            $userquestion = $question["#Id_profil"] - 1;
+            $query = $con->prepare("SELECT * FROM `profil` WHERE `Id_profil` = ( SELECT `#Id_profil` FROM `question` WHERE `Id_question` = $idquestion AND `#Id_profil` = $idprofil )");
+            $query->execute();
+            $users = $query->fetchAll();
+
+            $query = $con->prepare("SELECT * FROM `categorie` WHERE `Id_categorie` = ( SELECT `#Id_categorie` FROM `question` WHERE `Id_question` = $idquestion AND `#Id_categorie` = $idcategorie )");
+            $query->execute();
+            $categorie = $query->fetchAll();
     ?>
-        <div class="card responsive-bootstrap-card m-card" id="questionpose<?php echo $reponsequestion; ?>">
-            <h5 class="card-header" id="reponse<?php echo $reponsequestion; ?>"><img class="picture-user-small" src="profile-picture/lythis.jpg" alt=""> <b><?php echo $users[$userquestion]['Pseudo_profil']; ?></b> a posé la question :</h5>
+        <div class="card responsive-bootstrap-card m-card" id="questionpose<?php echo $idquestion ?>">
+            <h5 class="card-header" id="reponse<?php echo $idquestion; ?>"><img class="picture-user-small" src="profile-picture/lythis.jpg" alt=""> <b><?php echo $users["0"]["Pseudo_profil"]; ?></b> a posé la question :</h5>
             <div class="card-body">
-                <h5 class="card-title">Catégorie : <?php echo $categorie[$categoriequestion]['Libelle_categorie']; ?></h5>
-                <p class="card-text"><?php echo $question['Titre_question']; ?></p>
+                <h5 class="card-title">Catégorie : <?php echo $categorie["0"]["Libelle_categorie"]; ?></h5>
+                <p class="card-text"><?php echo $question["Titre_question"]; ?></p>
                 <blockquote class="blockquote mb-2">
                     <p></p>
-                    <footer class="blockquote-footer">Le <?php echo $question['Date_creation_question']; ?></footer>
+                    <footer class="blockquote-footer">Le <?php echo $question["Date_creation_question"]; ?></footer>
                 </blockquote>
-                <button class="btn btn-primary toggle-btn" type="button" data-toggle="collapse" data-target="#question<?php echo $reponsequestion; ?>" aria-expanded="false" aria-controls="question<?php echo $reponsequestion; ?>">
+                <button class="btn btn-primary toggle-btn" type="button" data-toggle="collapse" data-target="#question<?php echo $idquestion; ?>" aria-expanded="false" aria-controls="question<?php echo $idquestion; ?>">
                     <span class="afficher">Afficher les réponses</span>
                     <span class="masquer">Masquer les réponses</span>
                 </button>
@@ -44,19 +43,24 @@
             if (!empty($reponses)) {
                 foreach ($reponses as $reponse) {
 
-                    $userreponse = $reponse["#Id_profil"] - 1;
+                    $unereponse = $reponse["Id_reponse"];
+
+                    $query = $con->prepare("SELECT * FROM `profil` WHERE `Id_profil` = ( SELECT `#Id_profil` FROM `reponse` WHERE `#Id_question` = $idquestion AND `Id_reponse` = $unereponse)");
+                    $query->execute();
+                    $users = $query->fetchAll();
+
         ?>
-                <div class="card responsive-bootstrap-card collapse" id="question<?php echo $reponsequestion; ?>">
+                <div class="card responsive-bootstrap-card collapse" id="question<?php echo $idquestion; ?>">
                     <div class="card-body">
                         <div class="">
                             <div class="card-header">
-                                <img class="picture-user-small" src="profile-picture/leo.jpg" alt=""> <b><?php echo $users[$userreponse]['Pseudo_profil']; ?></b> a répondu :
+                                <img class="picture-user-small" src="profile-picture/leo.jpg" alt=""> <b><?php echo $users["0"]["Pseudo_profil"]; ?></b> a répondu :
                             </div>
                             <div class="card-body">
-                                <?php echo $reponse['Contenu_reponse']; ?>
+                                <?php echo $reponse["Contenu_reponse"]; ?>
                                 <blockquote class="blockquote mb-0">
                                     <p></p>
-                                    <footer class="blockquote-footer">Le <?php echo $reponse['Date_reponse']; ?></footer>
+                                    <footer class="blockquote-footer">Le <?php echo $reponse["Date_reponse"]; ?></footer>
                                 </blockquote>
                             </div>
                         </div>
@@ -69,7 +73,7 @@
             else {
     ?>
 
-        <div class="card responsive-bootstrap-card collapse" id="question<?php echo $reponsequestion; ?>">
+        <div class="card responsive-bootstrap-card collapse" id="question<?php echo $idquestion; ?>">
             <p>Wow, such empty.</p>
         </div>
 
