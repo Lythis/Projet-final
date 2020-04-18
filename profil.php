13f926adac5af624f74c5ad723f6d0fc0d7546a5
NicoYam <givsoul@gmail.com>
2020-04-18 15:38:16 +0200

    PHP pour edit du profil

--- profil.php
+++ profil.php
@@ -1,11 +1,23 @@
 <?php
+    //Connexion à la base de données
     require_once('db/base_PDO.php');
 
-    $gettheprofil = $_GET['profil'];
-    $query = $con->prepare("SELECT * FROM profil WHERE `Id_profil` = $gettheprofil");
-    $query->execute();
-    $users = $query->fetchAll();
+    //On a bien recupéré des données en GET
+    if(!empty($_GET['profil'])) {
+        $profilstatus = $_GET['profil'];
+        //Séparation du GET en 2 à partir de la virgule (création d'un tableau à 2 colonnes si on a bien une virgule, sinon une seule colonne)
+        $profilstatus = explode(',', $profilstatus);
+    }
 
+    //Ici $profilstatus[0] == la première colonne du tableau qu'on a recupéré (donc de notre GET) || Est-ce qu'on a bien un profil présent dans le GET?
+    if(isset($profilstatus[0])) {
+        $gettheprofil = $profilstatus[0];
+        $query = $con->prepare("SELECT * FROM profil WHERE `Id_profil` = $gettheprofil");
+        $query->execute();
+        $users = $query->fetchAll();
+    }
+
+    //Notre GET n'est pas vide et on a récupéré les informations de l'utilisateur présent dans notre GET (ici présent dans $users, donc si $users n'est pas vide)
     if(!empty($_GET['profil']) && !empty($users)) {
 
         $idprofil = $users[0]["Id_profil"];
@@ -17,7 +29,10 @@
 
         $title ='Profil de '.$users[0]["Pseudo_profil"];
         require_once('includes/header.php');
-    
+    }
+
+    //Notre tableau GET ($profilstatus) ne contient qu'une seule colonne
+    if(!empty($_GET['profil']) && !empty($users) && !isset($profilstatus[1])) {
 ?>
 
 <body>
@@ -34,6 +49,9 @@
                     <h3 class="card-title ml-2"><?php echo $users[0]["Pseudo_profil"]; ?></h3>
                     <h5>Genre : <?php echo $users[0]["Genre_profil"]; ?></h5>
                     <p><?php echo $users[0]["Description_profil"]; ?></p>
+                    <form action="./profil.php" method="get">
+                        <button name="profil" value="<?php echo $users[0]["Id_profil"]; ?>,edit">Editer le profil</button>
+                    </form>
                 </div>
 
             </div>
@@ -62,6 +80,38 @@
     </div>
     <?php
     }
+
+    //Une requête pour modifier le profil est présente (on a donc 2 colonnes dans notre GET ($profilstatus))
+    elseif(!empty($_GET['profil']) && !empty($users) && isset($profilstatus[1])) {
+        //La requête est-elle bien égale à "edit"?
+        if($profilstatus[1] == "edit") {
+            //Le profil demandé est le profil actuel de la session ou la session est administrateur
+            if($_SESSION['utilisateur']['id'] == $profilstatus[0] || $_SESSION['utilisateur']['role'] == 1) {
+                $title = 'Modification du profil de '.$users[0]["Pseudo_profil"];
+                require_once('includes/header.php');
+                require_once('includes/nav-bar.php');
+                ?>
+                    <!-- edit du profil ici -->
+                <?php
+            }
+            //Message d'erreur car accès refusé
+            else {
+                $title = 'Accès refusé';
+                require_once('includes/header.php');
+                require_once('includes/nav-bar.php');
+                echo 'Vous n\'avez pas le droit de modifier ce profil. <a href="./index.php">Revenir à l\'accueil</a>.';
+            }
+        }
+        //La requête n'est pas égale à "edit"
+        else {
+            $title = 'Erreur';
+            require_once('includes/header.php');
+            require_once('includes/nav-bar.php');
+            echo 'Problème lors de votre requête. <a href="./index.php">Revenir à l\'accueil</a>.';
+        }
+    }
+
+    //Message d'erreur classique si le profil est introuvable ou n'importe quel autre problème
     else {
         $title = 'Profil introuvable';
         require_once('includes/header.php');
@@ -69,8 +119,3 @@
         echo 'Profil introuvable. <a href="./index.php">Revenir à l\'accueil</a>.';
     }
     ?>
-    <div class="bas-page">
-    <?php
-        require_once('includes/footer.php');
-    ?>
-    </div>
