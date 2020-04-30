8dc65fc4b4dd916925a3e331260406a2f1f002cd
Lythis <morgan60127@gmail.com>
2020-04-30 20:35:46 +0200

    navbar non co toggler visible

--- includes/nav_bar.php
+++ includes/nav_bar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg couleur ">
     <a class="titre text-white" href="./index.php">Saint Vincent BTS 1</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="btn-nav" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse text-white" id="navbarNavDropdown">
