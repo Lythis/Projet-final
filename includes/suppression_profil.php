29deb5076ef72f62b8ece722050f0a946815fc27
Kyllian <lj.kyllian@gmail.com>
2020-04-30 18:44:20 +0200

    modif du responsif

--- includes/suppression_profil.php
+++ includes/suppression_profil.php
@@ -1,13 +1,13 @@
 <body class="bgP">
-    <div class="card d-flex">
+    <div class="pCard d-flex">
         <div class="card-body ">
             <img src="image/sad.gif" class="card-img-top " alt="triste">
-            <p class="card-text text-center ml-5 ">Voulez vous vraiment supprimer ce compte?
-                <form class="text-center" action="./delete.php" method="post">
-                    <button class="btn btn-danger mt-3" name="profil" value="<?php echo $profilStatus[0]; ?>">Supprimer</button>
+            <p class="card-text  ml-5 ">Voulez vous vraiment supprimer ce compte?
+                <form action="./delete.php" method="post">
+                    <button class="btn btn-danger mt-3" name="profil" value="<?php echo $profilStatus[0]; ?>" style="margin-left: 36%;">Supprimer</button>
                 </form>
-                <form class="text-center" action="./profil.php?profil=<?php echo $profilStatus[0]; ?>">
-                    <button class="btn btn-primary mt-3" name="profil" value="<?php echo $profilStatus[0]; ?>">Revenir en arrière</button>
+                <form action="./profil.php?profil=<?php echo $profilStatus[0]; ?>">
+                    <button class="btn btn-primary mt-3" name="profil" value="<?php echo $profilStatus[0]; ?>" style="margin-left: 28%;">Revenir en arrière</button>
                 </form>
             </div>
         </div>
--- includes/suppression_question.php
+++ includes/suppression_question.php
@@ -1,13 +1,13 @@
 <body class="bgP">
-    <div class="card d-flex">
+    <div class="pCard d-flex">
         <div class="card-body ">
             <img src="image/sad.gif" class="card-img-top " alt="triste">
 
-            <p class="card-text float-right ">Voulez vous vraiment supprimer cette question?
+            <p class="card-text" style="margin-left: 13%;">Voulez vous vraiment supprimer cette question?
                 <form action="./delete.php" method="post">
-                    <button class="btn btn-danger mt-3" name="question" value="<?php echo $questionStatus[0]; ?>">Supprimer</button>
+                    <button class="btn btn-danger mt-3" name="question" value="<?php echo $questionStatus[0]; ?>"style="margin-left: 36%;" >Supprimer</button>
                 </form>
-                <a href="./QuestionsReponses.php?question=<?php echo $questionStatus[0]; ?>" class="btn btn-primary mt-3">Revenir en arrière</a>
+                <a href="./QuestionsReponses.php?question=<?php echo $questionStatus[0]; ?>" class="btn btn-primary mt-3" style="margin-left: 28%;">Revenir en arrière</a>
             </div>
         </div>
         
