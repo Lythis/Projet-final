74ac5bdd44d85fb76680a81b8c34944dc734c346
prxph3te <61044926+prxph3te@users.noreply.github.com>
2020-06-20 15:49:38 +0200

    Update base_PDO.php

--- db/base_PDO.php
+++ db/base_PDO.php
@@ -2,7 +2,7 @@
 	#Fonction pour accéder à la base de données
 	function connexionBdd() {
 		$user = "root";
-		$pass = "root";
+		$pass = "";
 		$dbName = "live_question";
 		return new \PDO("mysql:host=127.0.0.1;dbname=$dbName;charset=UTF8", $user, $pass);
 	}
