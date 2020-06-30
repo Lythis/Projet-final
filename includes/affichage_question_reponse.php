5ad25267e168697525e49dad42baaefd80c7aada
Kyllian <lj.kyllian@gmail.com>
2020-06-30 14:41:09 +0200

    modif des messages de validation

--- includes/affichage_question_reponse.php
+++ includes/affichage_question_reponse.php
@@ -80,6 +80,7 @@
                 </form>
             </div>
         </div>
+    </div>
             <?php
             
             if (!empty($reponses)) {
--- includes/poser_question.php
+++ includes/poser_question.php
@@ -3,18 +3,17 @@
         <div class="card-body">
             <div id="rafraiche">
                 <div class="form-group">
-                    <form method="post" action="Questions.php" novalidate>
+                    <form class="needs-validation" method="post" action="Questions.php" novalidate>
                         <div class="form-group">
                             <div class="col-lg ">
                                 <label for="validationTooltip01">Votre question :</label>
                                 <textarea id="question" type="text" class="form-control autoExpand w-100 h-50" placeholder="" name="question" maxlength="250" required></textarea>
-                                
                                 <p style="text-align:right" id="compteur">0 mots | 0 Caractere / 250</p>
                                 <div class="invalid-feedback mb-2">
                                     Veuillez saisir une question.
                                 </div>
                             </div>
-                            <div class="col-lg needs-validation">
+                            <div class="col-lg">
                                 <label for="validationTooltip02">Catégorie :</label>
                                 <select class="custom-select mb-2" id="validationTooltip02" placeholder="Categorie" name="categorie" required>
                                     <option value="">Selectionner une catégorie</option>
